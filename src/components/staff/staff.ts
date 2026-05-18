116085b813b1a306c9c690f2c28331d4a1f509e8
AlexThomHab <173417632+AlexThomHab@users.noreply.github.com>
2026-05-18 14:58:33 +0100

    you can now add notes

--- src/components/staff/staff.ts
+++ src/components/staff/staff.ts
@@ -11,18 +11,31 @@ import {Note} from '../../models/note';
 export class Staff {
   @ViewChild('staffContainer')
   private staffContainer!: ElementRef;
-  private stave! : Stave;
-
-  private toVexKey(note: Note): string {
-    return note.noteValue.toLowerCase() + '/' + note.pitchClass;
-  }
+  private stave!: Stave;
 
   @Input() cantusFirmus: Note[] = [];
-  @Input() counterpoint: Note[] = [];
+  @Input() counterpoint: (Note | null)[] = Array(10).fill(null);
 
   ngAfterViewInit() {
-    const {Renderer, Stave, StaveNote, Voice, Formatter} = Vex;
-    const notes = [
+    this.drawExercise(this.counterpoint)
+    this.staffContainer.nativeElement.addEventListener('click', (e: MouseEvent) => {
+      this.addNote(e.clientY)
+    })
+  }
+
+  private drawExercise(counterPointInput: (Note | null)[]): void {
+    this.staffContainer.nativeElement.innerHTML = "";
+
+    const {Renderer, Stave, StaveNote, Voice, Formatter, GhostNote} = Vex;
+    const width = this.staffContainer.nativeElement.clientWidth;
+    const renderer = new Renderer(this.staffContainer.nativeElement, Renderer.Backends.SVG);
+    const context = renderer.getContext(); //context is the canvas that renderer renders to then draw on
+
+    renderer.resize(width, 250);
+    const cantusFirmusVoice = new Voice({numBeats: 10, beatValue: 1})
+    cantusFirmusVoice.setStrict(false)
+
+    const cantusFirmusNotes = [
       new StaveNote({keys: ['d/4'], duration: 'w'}),
       new StaveNote({keys: ['e/4'], duration: 'w'}),
       new StaveNote({keys: ['f/4'], duration: 'w'}),
@@ -34,38 +47,31 @@ export class Staff {
       new StaveNote({keys: ['G/4'], duration: 'w'}),
       new StaveNote({keys: ['a/4'], duration: 'w'})
     ]
-    const voice = new Voice({numBeats: 10, beatValue: 1}) //3 beats each beat value is worth one StaveNote
-    voice.setStrict(false) //we don't need to have the notes = to number of beats
-    voice.addTickables(notes)
 
-    const renderer = new Renderer(this.staffContainer.nativeElement, Renderer.Backends.SVG);
-    const width = this.staffContainer.nativeElement.clientWidth;
-    renderer.resize(width, 200);
-    const context = renderer.getContext(); //context is the canvas that renderer renders to then draw on
-    this.stave = new Stave(10, 40, width - 30);
+    this.stave = new Stave(10, 80, width - 30);
     this.stave.addClef('treble');
-    new Formatter().joinVoices([voice]).format([voice], this.stave.getWidth() - 60)
-    this.stave.setContext(context).draw(); //create staff with clef THEN we add CF
-    voice.draw(context, this.stave);
+    this.stave.setContext(context).draw();
 
-    const svg = this.staffContainer.nativeElement.querySelector('svg');
-    svg.addEventListener('click', (e: MouseEvent) => {
-      let xAxis = e.clientX
-      let yAxis = e.clientY
-      this.addNote(xAxis, yAxis)
-    })
+    cantusFirmusVoice.addTickables(cantusFirmusNotes)
+    new Formatter().joinVoices([cantusFirmusVoice]).format([cantusFirmusVoice], this.stave.getWidth() - 60)
+    cantusFirmusVoice.draw(context, this.stave);
+
+    const counterpointVoice = new Voice({numBeats: 10, beatValue: 1})
+    counterpointVoice.setStrict(false)
+    counterpointVoice.addTickables(
+      counterPointInput.map(note =>
+        note == null
+          ? new GhostNote({duration: 'w'})
+          : new StaveNote({keys: [this.toVexKey(note)], duration: 'w'})
+      )
+    )
+    new Formatter().joinVoices([counterpointVoice]).format([counterpointVoice], this.stave.getWidth() - 60)
+    counterpointVoice.draw(context, this.stave);
   }
 
-  private addNote(clickXAxis: number, clickYAxis: number) {
+  private addNote(clickYAxis: number) {
     const svg = this.staffContainer.nativeElement.querySelector('svg');
-    let eLineYAxis = this.stave.getBottomLineY(); //lowest line on treble clef(E)
-    let yDifferenceBetweenTwoLines =this.stave.getSpacingBetweenLines(); //lowest line on treble clef(E)
-    //wanna split a gap into quarters. e.g F in FACE, lower quarter will go to E, 2 middle will go to F and then upper will go to G like in Tonesavvy
     let listOfPossibleInputNotes: Note[] = [
-     /* new Note("A", 3),
-      new Note("B", 3),
-      new Note("C", 4),
-      new Note("D", 4),*/
       new Note("E", 4),
       new Note("F", 4),
       new Note("G", 4),
@@ -78,21 +84,18 @@ export class Staff {
       new Note("G", 5),
       new Note("A", 5)
     ];
-    const rect = svg.getBoundingClientRect(); //convert clickYAxis to svgClickY because we only get the axis relative to the context not viewport
+    const rect = svg.getBoundingClientRect();
     clickYAxis = clickYAxis - rect.top;
     let noteLineValue = this.stave.getLineForY(clickYAxis)
     let noteIndexRounded = Math.round(noteLineValue * 2) / 2
     let noteIndexInList = (4 - noteIndexRounded) / 0.5
     let inputNote = listOfPossibleInputNotes[noteIndexInList]
-    this.counterpoint.push(inputNote)
-    const {StaveNote, Formatter, Voice, Renderer} = Vex;
+    if (!inputNote) return;
+    this.counterpoint[this.counterpoint.indexOf(null)] = inputNote;
+    this.drawExercise(this.counterpoint)
+  }
 
-    const voice = new Voice({numBeats: 10, beatValue: 1}) //3 beats each beat value is worth one StaveNote
-    voice.setStrict(false) //we don't need to have the notes = to number of beats
-    voice.addTickables([new StaveNote({keys: [this.toVexKey(inputNote)], duration: 'w'})])
-    new Formatter().joinVoices([voice]).format([voice], this.stave.getWidth() - 60)
-    const renderer = new Renderer(this.staffContainer.nativeElement, Renderer.Backends.SVG);
-    const context = renderer.getContext();
-    voice.draw(context, this.stave);
+  private toVexKey(note: Note): string {
+    return note.noteValue.toLowerCase() + '/' + note.pitchClass;
   }
 }
